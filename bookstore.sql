a25ed7cf7dda04ab24074e3894d800af1f42bda7
Anne Ragu <161914768+Annareyy@users.noreply.github.com>
2025-04-12 18:10:33 +0300

    Update bookstore.sql

--- bookstore.sql
+++ bookstore.sql
@@ -80,7 +80,7 @@ CREATE TABLE shipping_method (
 );
 
 CREATE TABLE order_status (
-    status_id INT PRIMARY KEY,
+    orderstatus_id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
@@ -89,10 +89,10 @@ CREATE TABLE cust_order (
     customer_id INT,
     order_date DATE,
     shipping_id INT,
-    status_id INT,
+    orderstatus_id INT,
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
     FOREIGN KEY (shipping_id) REFERENCES shipping_method(shipping_id),
-    FOREIGN KEY (status_id) REFERENCES order_status(status_id)
+    FOREIGN KEY (orderstatus_id) REFERENCES order_status(orderstatus_id)
 );
 
 CREATE TABLE order_line (
@@ -108,9 +108,9 @@ CREATE TABLE order_line (
 CREATE TABLE order_history (
     history_id INT PRIMARY KEY,
     order_id INT,
-    status_id INT,
+    orderstatus_id INT,
     updated_at DATETIME,
     FOREIGN KEY (order_id) REFERENCES cust_order(order_id),
-    FOREIGN KEY (status_id) REFERENCES order_status(status_id)
+    FOREIGN KEY (orderstatus_id) REFERENCES order_status(orderstatus_id)
 );
 
